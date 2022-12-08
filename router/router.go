41e8f58b77648366823863f83aec72eb804bb271
SHIVENDU TYAGI <88075377+shivendutyagi@users.noreply.github.com>
2022-12-08 09:03:04 +0530

    Add files via upload

--- router/router.go
+++ router/router.go
@@ -1,1 +1,16 @@
+package router
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/shivendutyagi/newapi/controllers"
+)
+
+func Router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("api/getmovie", controllers.Getallmovies).Methods("GET")
+	r.HandleFunc("api/create", controllers.Createmovie).Methods("POST")
+	r.HandleFunc("api/getmovie/{id}", controllers.Marksaswatched).Methods("PUT")
+	r.HandleFunc("api/getmovie/{id}", controllers.Deleteamovie).Methods("DELETE")
+
+	return r
+}
